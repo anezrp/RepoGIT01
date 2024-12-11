60462983aee43b2f8879d9d9a3e9901b474b5147
Ane Pérez <ane.perez05@somo.eus>
2024-12-11 09:50:58 +0100

    Primer commit

---
+++ src/clasesDatos/Empleado.java
@@ -0,0 +1,11 @@
+package clasesDatos;
+
+public class Empleado {
+	String dni;
+	String nombre;
+	String apellidos;
+	boolean clubSocial; //Indica si el empleado está en el club social de la empresa
+	float sueldo;
+	
+	
+}
