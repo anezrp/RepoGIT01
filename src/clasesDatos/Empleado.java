52265ab6fa4959a88d334d45de40720337c42354
Ane Pérez <ane.perez05@somo.eus>
2024-12-18 09:22:58 +0100

    Añadido toString y otros cambios

--- src/clasesDatos/Empleado.java
+++ src/clasesDatos/Empleado.java
@@ -1,12 +1,106 @@
 package clasesDatos;
 
 public class Empleado {
-	private String dni;
-	private String nombre;
-	private String apellidos;
-	private String categoria;
-	private int edad;
-	private boolean clubSocial; //Indica si el empleado está en el club social de la empresa
-	private float sueldo;
-	
+private String dni;
+private String nombre;
+private String apellidos;
+private String categ;
+private int edad;
+private boolean clubSocial; // Indica si el empleado esta en el club social de la empresa
+private float sueldo;
+
+
+/**
+ * @param dni
+ * @param nombre
+ * @param apellidos
+ * @param categ
+ * @param edad
+ * @param clubSocial
+ * @param sueldo
+ */
+//Constructor completo
+public Empleado(String dni, String nombre, String apellidos, String categ, int edad, boolean clubSocial,
+float sueldo) {
+super();
+this.dni = dni;
+this.nombre = nombre;
+this.apellidos = apellidos;
+this.categ = categ;
+this.edad = edad;
+this.clubSocial = clubSocial;
+this.sueldo = sueldo;
+}
+
+//Constructor vacio
+public Empleado() {
+super();
+dni = "";
+nombre = "***********";
+apellidos = "*********";
+categ = "Base";
+this.edad = 0;
+this.clubSocial = false;
+this.sueldo = (float) 0.0;
+}
+//Constructor parcial
+/**
+ * @param dni
+ * @param nombre
+ * @param apellidos
+ */
+public Empleado(String dni, String nombre, String apellidos) {
+super();
+this.dni = dni;
+this.nombre = nombre;
+this.apellidos = apellidos;
+
+//Getters y setters
+}
+public String getDni() {
+return dni;
+}
+public void setDni(String dni) {
+this.dni = dni;
+}
+public String getNombre() {
+return nombre;
+}
+public void setNombre(String nombre) {
+this.nombre = nombre;
+}
+public String getApellidos() {
+return apellidos;
+}
+public void setApellidos(String apellidos) {
+this.apellidos = apellidos;
+}
+public String getCateg() {
+return categ;
+}
+public void setCateg(String categ) {
+this.categ = categ;
+}
+public int getEdad() {
+return edad;
+}
+public void setEdad(int edad) {
+this.edad = edad;
+}
+public boolean isClubSocial() {
+return clubSocial;
+}
+public void setClubSocial(boolean clubSocial) {
+this.clubSocial = clubSocial;
+}
+public float getSueldo() {
+return sueldo;
+}
+public void setSueldo(float sueldo) {
+this.sueldo = sueldo;
+}
+
+public String toString() {
+	return "Empleado [dni"+dni+", nombre="+nombre+", apellidos="+apellidos+", categoria="+categ+", edad="+edad+", club social= "+clubSocial+", sueldo="+sueldo;
+}
 }
